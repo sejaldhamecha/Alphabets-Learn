37d2c28f5a8a1fe40972569b1c9a50cbccbe88fa
sejaldhamecha <cse22.sejaldhamecha@ggct.co.in>
2024-12-21 12:59:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,155 @@
+const englishUppercase = [
+    { letter: 'A', sound: 'apple.mp3', image: 'apple.jpg' },
+    { letter: 'B', sound: 'BALL.mp3', image: 'ball.jpg' },
+    { letter: 'C', sound: 'C.mp3', image: 'cat.jpg' },
+    { letter: 'D', sound: 'dog.mp3', image: 'dog.jpg' },
+    { letter: 'E', sound: 'elephant.mp3', image: 'elephant.jpg' },
+    { letter: 'F', sound: 'fish.mp3', image: 'fish.jpg' },
+    { letter: 'G', sound: 'G.mp3', image: 'grapes.jpg' },
+    { letter: 'H', sound: 'H.mp3', image: 'horse.jpg' },
+    { letter: 'I', sound: 'I.mp3', image: 'icecream.jpg' },
+    { letter: 'J', sound: 'juice.mp3', image: 'joker.jpg' },
+    { letter: 'K', sound: 'Kangaroo.mp3', image: 'kite.jpg' },
+    { letter: 'L', sound: 'L.mp3', image: 'lion.jpg' },
+    { letter: 'M', sound: 'monkey.mp3', image: 'monkey.jpg' },
+    { letter: 'N', sound: 'N.mp3', image: 'iowl.jpg' },
+    { letter: 'P', sound: 'P.mp3', image: 'parrot.jpg' },
+    { letter: 'Q', sound: 'Q.mp3', image: 'queen.jpg' },
+    { letter: 'R', sound: 'R.mp3', image: 'rat.jpg' },
+    { letter: 'S', sound: 'S.mp3', image: 'sun.jpg' },
+    { letter: 'T', sound: 'T.mp3', image: 'train.jpg' },
+    { letter: 'U', sound: 'U.mp3', image: 'umbrella.webp' },
+    { letter: 'V', sound: 'V.mp3', image: 'ven.jpg' },
+    { letter: 'W', sound: 'W.mp3', image: 'watch.jpg' },
+    { letter: 'X', sound: 'X.mp3', image: 'xmass.jpg' },
+    { letter: 'Y', sound: 'Y.mp3', image: 'yark.jpg' },
+    { letter: 'Z', sound: 'Z.mp3', image: 'zebra.jpg' },
+  ];
+  const englishLowercase = [
+    { letter: 'a', sound: 'apple.mp3', image: 'a.png' },
+    { letter: 'b', sound: 'BALL.mp3', image: 'b.png' },
+    { letter: 'c', sound: 'assets/sounds/c.mp3', image: 'c.png' },
+    { letter: 'd', sound: 'd.mp3', image: 'assets/images/d.png' },
+    { letter: 'e', sound: 'e.mp3', image: 'assets/images/e.png' },
+    { letter: 'f', sound: 'f.mp3', image: 'assets/images/f.png' },
+    { letter: 'g', sound: 'g.mp3', image: 'assets/images/g.png' },
+    { letter: 'h', sound: 'h.mp3', image: 'assets/images/h.png' },
+    { letter: 'i', sound: 'assets/sounds/i.mp3', image: 'assets/images/i.png' },
+    { letter: 'j', sound: 'assets/sounds/j.mp3', image: 'assets/images/j.png' },
+    { letter: 'k', sound: 'assets/sounds/k.mp3', image: 'assets/images/k.png' },
+    { letter: 'l', sound: 'assets/sounds/l.mp3', image: 'assets/images/l.png' },
+    { letter: 'm', sound: 'assets/sounds/m.mp3', image: 'assets/images/m.png' },
+    { letter: 'n', sound: 'assets/sounds/n.mp3', image: 'assets/images/n.png' },
+    { letter: 'o', sound: 'assets/sounds/o.mp3', image: 'assets/images/o.png' },
+    { letter: 'p', sound: 'assets/sounds/p.mp3', image: 'assets/images/p.png' },
+    { letter: 'q', sound: 'assets/sounds/q.mp3', image: 'assets/images/q.png' },
+    { letter: 'r', sound: 'assets/sounds/r.mp3', image: 'assets/images/r.png' },
+    { letter: 's', sound: 'assets/sounds/s.mp3', image: 'assets/images/s.png' },
+    { letter: 't', sound: 'assets/sounds/t.mp3', image: 'assets/images/t.png' },
+    { letter: 'u', sound: 'assets/sounds/u.mp3', image: 'assets/images/u.png' },
+    { letter: 'v', sound: 'assets/sounds/v.mp3', image: 'assets/images/v.png' },
+    { letter: 'w', sound: 'assets/sounds/w.mp3', image: 'assets/images/w.png' },
+    { letter: 'x', sound: 'assets/sounds/x.mp3', image: 'assets/images/x.png' },
+    { letter: 'y', sound: 'assets/sounds/y.mp3', image: 'assets/images/y.png' },
+    { letter: 'z', sound: 'assets/sounds/z.mp3', image: 'assets/images/z.png' },
+  ];
+  const swar = [
+    { letter: 'अ', sound: 'ANAAR.mp3', image: 'annar.jpg' },
+    { letter: 'आ', sound: 'AAM.mp3', image: 'aam.jpg' },
+    { letter: 'इ', sound: 'इ.mp3', image: 'imli.jpg' },
+    { letter: 'ई', sound: 'ई.mp3', image: 'Iikh.jpg' },
+    { letter: 'उ', sound: 'उ.mp3', image: 'owl.jpg' },
+    { letter: 'ऊ', sound: 'ऊ.mp3', image: 'images/' },
+    { letter: 'ए', sound: 'ए.mp3', image:'aidi (ankle).png' },
+    { letter: 'ऐ', sound: 'ऐ.mp3', image: 'ainak.jpg' },
+    { letter: 'ओ', sound: 'ओ.mp3', image:'okhali.jpg' },
+    { letter: 'औ', sound: 'औ.mp3', image:'aurat.jpg' },
+    { letter: 'अं', sound: 'khali..mp3', image:'angah.jpg' },
+    { letter: 'अः', sound: 'अः.mp3', image:'.png' },
+  ];
+  
+  const vyanjan = [
+    { letter: 'क', sound: 'KABOOTAR.mp3', image: 'kabutar.jpg' },
+    { letter: 'ख', sound: 'KHARGOSH.mp3', image: 'khargosh.jpg' },
+    { letter: 'ग', sound: 'cow..mp3', image:' gamla.jpg' },
+    { letter: 'घ', sound: 'gadhi..mp3', image: 'ghadi.jpg' },
+    { letter: 'ङ', sound: 'ङ.mp3', image: 'images/' },
+    { letter: 'च', sound: 'च.mp3', image: 'chammach.jpg' },
+    { letter: 'छ', sound: 'छ.mp3', image: 'chatta.jpg' },
+    { letter: 'ज', sound: 'ज.mp3', image: 'ज.png' },
+    { letter: 'झ', sound: 'झ.mp3', image: 'flag.jpg' },
+    { letter: 'ञ', sound: 'ञ.mp3', image: 'iyan.jpg' },
+    { letter: 'ट', sound: 'assets/sounds/ट.mp3', image: 'ट.png' },
+    { letter: 'ठ', sound: 'thanda.mp3', image: 'ठ.png' },
+    { letter: 'ड', sound: 'assets/sounds/ड.mp3', image: 'damro.jpg' },
+    { letter: 'ढ', sound: 'assets/sounds/ढ.mp3', image: 'dhakkan.jpg' },
+    { letter: 'ण', sound: 'assets/sounds/ण.mp3', image: 'ण.png' },
+    { letter: 'त', sound: 'assets/sounds/त.mp3', image: 'त.png' },
+    { letter: 'थ', sound: 'assets/sounds/थ.mp3', image: 'थ.png' },
+    { letter: 'द', sound: 'assets/sounds/द.mp3', image: 'dawai.jpg' },
+    { letter: 'ध', sound: 'assets/sounds/ध.mp3', image: 'dhanush.jpg' },
+    { letter: 'न', sound: 'assets/sounds/न.mp3', image: 'nose.jpg' },
+    { letter: 'प', sound: 'assets/sounds/प.mp3', image: 'patang.jpg' },
+    { letter: 'फ', sound: 'assets/sounds/फ.mp3', image: 'फ.png' },
+    { letter: 'ब', sound: 'assets/sounds/ब.mp3', image: 'bakri.jpg' },
+    { letter: 'भ', sound: 'assets/sounds/भ.mp3', image: 'bhalu.jpg' },
+    { letter: 'म', sound: 'assets/sounds/म.mp3', image: 'म.png' },
+    { letter: 'य', sound: 'assets/sounds/य.mp3', image: 'य.png' },
+    { letter: 'र', sound: 'assets/sounds/र.mp3', image: 'rassii.jpg' },
+    { letter: 'ल', sound: 'assets/sounds/ल.mp3', image: 'ल.png' },
+    { letter: 'व', sound: 'assets/sounds/व.mp3', image: 'व.png' },
+    { letter: 'श', sound: 'assets/sounds/श.mp3', image: 'श.png' },
+    { letter: 'ष', sound: 'assets/sounds/ष.mp3', image: 'ष.png' },
+    { letter: 'स', sound: 'assets/sounds/स.mp3', image: 'aapera.jpg' },
+    { letter: 'ह', sound: 'assets/sounds/ह.mp3', image: 'ह.png' },
+    { letter: 'क्ष', sound: 'assets/sounds/क्ष.mp3', image: '' },
+    { letter: 'त्र', sound: 'assets/sounds/त्र.mp3', image: 'त्र.png' },
+    { letter: 'ज्ञ', sound: 'assets/sounds/ज्ञ.mp3', image: 'gyani.jpg' },
+  ];
+  
+  
+  const englishBtn = document.getElementById('englishBtn');
+  const Capital = document.getElementById('CapitalBtn');
+  const small = document.getElementById('smallBtn');
+  const hindiBtn = document.getElementById('hindiBtn');
+  const swarBtn = document.getElementById('swarBtn');
+  const vyanjanBtn = document.getElementById('vyanjanBtn');
+  const hindiSections = document.getElementById('hindi-sections');
+  const englishSections = document.getElementById('english-sections');
+  const lettersContainer = document.getElementById('letters-container');
+  
+  function playSound(sound) {
+    const audio = new Audio(sound);
+    audio.play();
+  }
+  
+  function displayLetters(letters) {
+    lettersContainer.innerHTML = ''; // Clear previous content
+    letters.forEach(({ letter, sound, image }) => {
+      const card = document.createElement('div');
+      card.className = 'letter-card';
+      card.innerHTML = `
+        <img src="${image}" alt="${letter}">
+        <h2>${letter}</h2>
+      `;
+      card.addEventListener('click', () => playSound(sound));
+      lettersContainer.appendChild(card);
+    });
+  }
+  
+  // Event Listeners
+  englishBtn.addEventListener('click', () => {
+    englishSections.classList.remove('hidden');
+    hindiSections.classList.add('hidden');
+    //  displayLetters([...englishUppercase, ...englishLowercase]);
+  });
+  
+  hindiBtn.addEventListener('click', () => {
+    hindiSections.classList.remove('hidden');
+    // displayLetters(swar); // Default to Swar
+  });
+  
+  swarBtn.addEventListener('click', () => displayLetters(swar));
+  vyanjanBtn.addEventListener('click', () => displayLetters(vyanjan));
+  Capital.addEventListener('click', () => displayLetters(englishUppercase));
+  small.addEventListener('click', () => displayLetters(englishLowercase));
